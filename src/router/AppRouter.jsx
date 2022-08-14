10c83d0b4cee8561fa703fcecb0d95166e499d6a
JhonCar <jhon7car@gmail.com>
2022-08-14 16:55:17 -0300

    fin

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,19 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { LoginPage } from "../auth"
+import { DcPage, HeroesRoutes, MarvelPage } from "../heroes"
+import { Navbar } from "../ui"
+
+
+export const AppRouter = () => {
+    return (
+        <>
+            <Routes>
+                
+                <Route path="/login" element={<LoginPage />} />
+
+                <Route path="/*" element={<HeroesRoutes />} />
+                
+            </Routes>
+        </>
+    )
+}
